bedb12cf8bef06576241f04d623ebd30646f2fb2
yvemula <yvemula@gmail.com>
2025-02-21 14:58:29 -0500

    Revert "homepage"
    This reverts commit d119cc3ef3409f36d2bbc01462f313a04744d861.

--- my-vite-project/src/App.jsx
+++ my-vite-project/src/App.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Navbar from "./Components/Navbar";
-import "./App.css";
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Navbar from "./Components/Navbar" 
+import './App.css'
 
 const App = () => {
-  const current_theme = localStorage.getItem("current_theme");
-  const [theme, setTheme] = useState(current_theme ? current_theme : "light");
 
-  useEffect(() => {
-    localStorage.setItem("current_theme", theme);
-  }, [theme]);
+  const current_theme = localStorage.getItem('current_theme');
+  const [theme, setTheme] = useState(current_theme ? current_theme : 'light');
+
+  useEffect(()=> {
+    localStorage.setItem('current_theme', theme); 
+  },[theme])
 
   return (
-    <div className={`container ${theme}`}>
-      <Navbar theme={theme} setTheme={setTheme} />
-      <HomePage />
-    </div>
-  );
-};
+  <div className={`container ${theme}`}> 
+    <Navbar theme={theme} setTheme={setTheme}/>
+      </div>
+  )
+}
 
-export default App;
+export default App
